65cf20a88ff85a7d66dd571514e0f2b64a905664
Blake Prall <127055195+bprall@users.noreply.github.com>
2023-12-08 17:15:01 -0500

    Update train.py

--- train/train.py
+++ train/train.py
@@ -1,1 +1,106 @@
+from model.model import SpeechRecognitionModel
+from test.test import test
 
+
+
+
+
+class IterMeter(object):
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
+
+
+def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment):
+    model.train()
+    data_len = len(train_loader.dataset)
+    with experiment.train():
+        for batch_idx, _data in enumerate(train_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(spectrograms)  # (batch, time, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1) # (time, batch, n_class)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+            loss.backward()
+
+            experiment.log_metric('loss', loss.item(), step=iter_meter.get())
+            experiment.log_metric('learning_rate', scheduler.get_lr(), step=iter_meter.get())
+
+            optimizer.step()
+            scheduler.step()
+            iter_meter.step()
+            if batch_idx % 100 == 0 or batch_idx == data_len:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(spectrograms), data_len,
+                    100. * batch_idx / len(train_loader), loss.item()))
+
+def main(learning_rate=5e-4, batch_size=20, epochs=10,
+        train_url="train-clean-100", test_url="test-clean",
+        experiment=Experiment(api_key='dummy_key', disabled=True)):
+
+    hparams = {
+        "n_cnn_layers": 3,
+        "n_rnn_layers": 5,
+        "rnn_dim": 512,
+        "n_class": 29,
+        "n_feats": 128,
+        "stride":2,
+        "dropout": 0.1,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "epochs": epochs
+    }
+
+    experiment.log_parameters(hparams)
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if not os.path.isdir("./data"):
+        os.makedirs("./data")
+
+    train_dataset = torchaudio.datasets.LIBRISPEECH("./data", url=train_url, download=True)
+    test_dataset = torchaudio.datasets.LIBRISPEECH("./data", url=test_url, download=True)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = data.DataLoader(dataset=train_dataset,
+                                batch_size=hparams['batch_size'],
+                                shuffle=True,
+                                collate_fn=lambda x: data_processing(x, 'train'),
+                                **kwargs)
+    test_loader = data.DataLoader(dataset=test_dataset,
+                                batch_size=hparams['batch_size'],
+                                shuffle=False,
+                                collate_fn=lambda x: data_processing(x, 'valid'),
+                                **kwargs)
+
+    model = SpeechRecognitionModel(
+        hparams['n_cnn_layers'], hparams['n_rnn_layers'], hparams['rnn_dim'],
+        hparams['n_class'], hparams['n_feats'], hparams['stride'], hparams['dropout']
+        ).to(device)
+
+    print(model)
+    print('Num Model Parameters', sum([param.nelement() for param in model.parameters()]))
+
+    optimizer = optim.Adam(model.parameters(), hparams['learning_rate'])
+    criterion = nn.CTCLoss(blank=28).to(device)
+    scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=hparams['learning_rate'],
+                                            steps_per_epoch=int(len(train_loader)),
+                                            epochs=hparams['epochs'],
+                                            anneal_strategy='linear')
+
+    iter_meter = IterMeter()
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
+        test(model, device, test_loader, criterion, epoch, iter_meter, experiment)
