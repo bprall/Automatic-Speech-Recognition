21d33097029d9731da07fd3f27e72bc93f5a830f
Blake Prall <127055195+bprall@users.noreply.github.com>
2023-12-08 17:19:24 -0500

    Update cer.py

--- utils/cer.py
+++ utils/cer.py
@@ -1,1 +1,60 @@
+import numpy as np
 
+def _levenshtein_distance(ref, hyp):
+    m = len(ref)
+    n = len(hyp)
+
+    if ref == hyp:
+        return 0
+    if m == 0:
+        return n
+    if n == 0:
+        return m
+
+    if m < n:
+        ref, hyp = hyp, ref
+        m, n = n, m
+
+    distance = np.zeros((2, n + 1), dtype=np.int32)
+
+    for j in range(0,n + 1):
+        distance[0][j] = j
+
+    for i in range(1, m + 1):
+        prev_row_idx = (i - 1) % 2
+        cur_row_idx = i % 2
+        distance[cur_row_idx][0] = i
+        for j in range(1, n + 1):
+            if ref[i - 1] == hyp[j - 1]:
+                distance[cur_row_idx][j] = distance[prev_row_idx][j - 1]
+            else:
+                s_num = distance[prev_row_idx][j - 1] + 1
+                i_num = distance[cur_row_idx][j - 1] + 1
+                d_num = distance[prev_row_idx][j] + 1
+                distance[cur_row_idx][j] = min(s_num, i_num, d_num)
+
+    return distance[m % 2][n]
+
+
+def char_errors(reference, hypothesis, ignore_case=False, remove_space=False):
+    if ignore_case == True:
+        reference = reference.lower()
+        hypothesis = hypothesis.lower()
+
+    join_char = ' '
+    if remove_space == True:
+        join_char = ''
+
+    reference = join_char.join(filter(None, reference.split(' ')))
+    hypothesis = join_char.join(filter(None, hypothesis.split(' ')))
+
+    edit_distance = _levenshtein_distance(reference, hypothesis)
+    return float(edit_distance), len(reference)
+  
+
+def cer(reference, hypothesis, ignore_case=False, remove_space=False):
+    edit_distance, ref_len = char_errors(reference, hypothesis, ignore_case, remove_space)
+    if ref_len == 0:
+        raise ValueError("Length of reference should be greater than 0.")
+    cer = float(edit_distance) / ref_len
+    return cer
