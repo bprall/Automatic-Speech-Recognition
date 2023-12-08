2527973ab954ba8afb211f5e094df6de526de775
Blake Prall <127055195+bprall@users.noreply.github.com>
2023-12-08 17:22:40 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,34 @@
+def test(model, device, test_loader, criterion, epoch, iter_meter):
+    print('\nevaluating...')
+    model.eval()
+    test_loss = 0
+    test_cer = []
+    test_wer = []
 
+    with torch.no_grad():
+        for i, _data in enumerate(test_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+            output = model(spectrograms)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+            test_loss += loss.item() / len(test_loader)
+
+            decoded_preds, decoded_targets = GreedyDecoder(output.transpose(0, 1), labels, label_lengths)
+
+            print("Decoded_pred len:", len(decoded_preds))
+            print("Decoded_targets len:", len(decoded_targets))
+
+            for j in range(len(decoded_preds)):
+                    test_cer.append(cer(decoded_targets[j], decoded_preds[j]))
+                    test_wer.append(wer(decoded_targets[j], decoded_preds[j]))
+                    print(len(decoded_preds[j]))
+                    print( len(decoded_targets[j]))
+
+    avg_cer = sum(test_cer) / len(test_cer)
+    avg_wer = sum(test_wer) / len(test_wer)
+
+    print('Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n'.format(test_loss, avg_cer, avg_wer))
