bfdb97fb5d60cfc12979f243e8594a3261fd4d75
Blake Prall <127055195+bprall@users.noreply.github.com>
2023-12-08 17:59:34 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,17 @@
+import os
+import torch.optim as optim
+import torch.utils.data as data
+from torch.utils.data import Dataset, DataLoader
+import torchaudio
+
 from utils.greedy import *
 from utils.wer import *
 from utils.cer import *
+
+NUM_EPOCHS = 10
+BATCH_SIZE = 10
+
+test_set = "test-clean"
 
 def test(model, device, test_loader, criterion, epoch, iter_meter):
     print('\nevaluating...')
@@ -36,3 +47,46 @@ def test(model, device, test_loader, criterion, epoch, iter_meter):
     avg_wer = sum(test_wer) / len(test_wer)
 
     print('Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n'.format(test_loss, avg_cer, avg_wer))
+
+
+def main(batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, test_url="test-clean"):
+    hparams = {
+        "n_cnn_layers": 3,
+        "n_lstm_layers": 5,
+        "lstm_dim": 256,
+        "n_class": 29,
+        "n_feats": 128,
+        "dropout": 0.1,
+        "batch_size": batch_size,
+        "epochs": epochs
+    }
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if not os.path.isdir("./data"):
+        os.makedirs("./data")
+
+    test_dataset = torchaudio.datasets.LIBRISPEECH("./data", url=test_url, download=True)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    
+    test_loader = data.DataLoader(dataset=test_dataset, batch_size=hparams['batch_size'],
+                                  shuffle=False, collate_fn=lambda x: data_processing(x, 'valid'), **kwargs)
+
+    model = SpeechRecognitionModel(
+        hparams['n_cnn_layers'], hparams['n_lstm_layers'], hparams['lstm_dim'],
+        hparams['n_class'], hparams['n_feats'], hparams['dropout']
+        ).to(device)
+
+    print(model)
+    print('Num Model Parameters', sum([param.nelement() for param in model.parameters()]))
+
+    criterion = nn.CTCLoss(blank=28).to(device)
+
+    iter_meter = IterMeter()
+    test(model, device, test_loader, criterion, epoch, iter_meter)
+
+if __name__ == '__main__':
+    main(BATCH_SIZE, NUM_EPOCHS, test_set)
