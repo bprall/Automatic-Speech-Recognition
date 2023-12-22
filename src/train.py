b1f5b4c78113524ba9e67ecca1bbb0406a73633a
Blake Prall <127055195+bprall@users.noreply.github.com>
2023-12-22 06:50:23 -0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
+import torch.nn as nn
+import torch.nn.functional as F
 
 from model import *
 from test import test
@@ -16,37 +18,31 @@ lr = 0.01
 train_set = "train-clean-100"
 test_set = "test-clean"
 
-def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment):
+def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter):
     model.train()
     data_len = len(train_loader.dataset)
-    with experiment.train():
-        for batch_idx, _data in enumerate(train_loader):
-            spectrograms, labels, input_lengths, label_lengths = _data
-            spectrograms, labels = spectrograms.to(device), labels.to(device)
+    for batch_idx, _data in enumerate(train_loader):
+        spectrograms, labels, input_lengths, label_lengths = _data
+        spectrograms, labels = spectrograms.to(device), labels.to(device)
 
-            optimizer.zero_grad()
+        optimizer.zero_grad()
 
-            output = model(spectrograms)  # (batch, time, n_class)
-            output = F.log_softmax(output, dim=2)
-            output = output.transpose(0, 1) # (time, batch, n_class)
+        output = model(spectrograms)
+        output = F.log_softmax(output, dim=2)
+        output = output.transpose(0, 1)
 
-            loss = criterion(output, labels, input_lengths, label_lengths)
-            loss.backward()
+        loss = criterion(output, labels, input_lengths, label_lengths)
+        loss.backward()
 
-            experiment.log_metric('loss', loss.item(), step=iter_meter.get())
-            experiment.log_metric('learning_rate', scheduler.get_lr(), step=iter_meter.get())
+        optimizer.step()
+        scheduler.step()
+        iter_meter.step()
+        if batch_idx % 100 == 0 or batch_idx == data_len:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(spectrograms), data_len,
+                100. * batch_idx / len(train_loader), loss.item()))
 
-            optimizer.step()
-            scheduler.step()
-            iter_meter.step()
-            if batch_idx % 100 == 0 or batch_idx == data_len:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(spectrograms), data_len,
-                    100. * batch_idx / len(train_loader), loss.item()))
-
-def main(learning_rate=5e-4, batch_size=20, epochs=10,
-        train_url="train-clean-100", test_url="test-clean",
-        experiment=Experiment(api_key='dummy_key', disabled=True)):
+def main(learning_rate=5e-4, batch_size=20, epochs=10, train_url="train-clean-100", test_url="test-clean"):
 
     hparams = {
         "n_cnn_layers": 3,
@@ -60,8 +56,6 @@ def main(learning_rate=5e-4, batch_size=20, epochs=10,
         "batch_size": batch_size,
         "epochs": epochs
     }
-
-    experiment.log_parameters(hparams)
 
     use_cuda = torch.cuda.is_available()
     torch.manual_seed(7)
@@ -102,8 +96,8 @@ def main(learning_rate=5e-4, batch_size=20, epochs=10,
 
     iter_meter = IterMeter()
     for epoch in range(1, epochs + 1):
-        train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
-        test(model, device, test_loader, criterion, epoch, iter_meter, experiment)
+        train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter)
+        test(model, device, test_loader, criterion, epoch, iter_meter)
 
         torch.save(model.state_dict(), 'final_speech_to_text_model.pth')
 
